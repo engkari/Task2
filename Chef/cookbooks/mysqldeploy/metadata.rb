ccd41807bc98ab92999208b7f2fe0f150db24036
engkari <engkari@users.noreply.github.com>
2016-04-04 14:57:00 +0300

    Screenshots and logs

---
+++ Chef/cookbooks/mysqldeploy/metadata.rb
@@ -0,0 +1,10 @@
+name             'mysqldeploy'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures mysqldeploy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends	'mysql', '~> 6.1.3'
+depends 'utilities'
